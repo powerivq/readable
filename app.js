441bff60283002936bc181a441ce4d503232d798
powerivq <poweriv.q@gmail.com>
2018-07-18 13:35:16 -0700

    Disable request debug

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const port = 80;
 const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
 
 http.globalAgent.keepAlive = true;
-rq.debug = true;
 
 const formatDigits = (num, digit) => {
   var ans = "" + num;
