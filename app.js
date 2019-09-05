212612a56750e3d8784cc91679d63048a8a677dd
powerivq <poweriv.q@gmail.com>
2019-09-04 21:32:17 -0700

    Make port a variable

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const http = require("http");
 const Readability = require("readability");
 const { JSDOM } = require("jsdom");
 
-const port = 80;
+const port = process.env.PORT || 80;
 const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
 
 http.globalAgent.keepAlive = true;
